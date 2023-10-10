91d5299a493780cea4f617cc9521931a0302a335
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-11 04:33:18 +0900

    add test for ExtensionsByType

--- mimedb_test.go
+++ mimedb_test.go
@@ -1,6 +1,9 @@
 package mimedb
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestTypeByExtension(t *testing.T) {
 	tests := []struct {
@@ -30,6 +33,38 @@ func TestTypeByExtension(t *testing.T) {
 		typ := TypeByExtension(test.ext)
 		if typ != test.typ {
 			t.Errorf("TypeByExtension(%q) = %q, want %q", test.ext, typ, test.typ)
+		}
+	}
+}
+
+func TestExtensionsByType(t *testing.T) {
+	tests := []struct {
+		typ string
+		ext []string
+	}{
+		// https://github.com/golang/go/blob/e7015c9327c4d755651ed3de3fd34fd99a479924/src/mime/type.go#L60-L77
+		{"image/avif", []string{".avif"}},
+		{"text/css", []string{".css"}},
+		{"image/gif", []string{".gif"}},
+		{"text/html", []string{".htm", ".html", ".shtml"}},
+		{"image/jpeg", []string{".jpe", ".jpeg", ".jpg"}},
+		{"application/javascript", []string{".js", ".mjs"}},
+		{"application/json", []string{".json", ".map"}},
+		{"application/pdf", []string{".pdf"}},
+		{"image/png", []string{".png"}},
+		{"image/svg+xml", []string{".svg", ".svgz"}},
+		{"application/wasm", []string{".wasm"}},
+		{"image/webp", []string{".webp"}},
+		{"text/xml", []string{".xml"}},
+	}
+
+	for _, test := range tests {
+		ext, err := ExtensionsByType(test.typ)
+		if err != nil {
+			t.Errorf("ExtensionsByType(%q) = %v", test.typ, err)
+		}
+		if !slices.Equal(ext, test.ext) {
+			t.Errorf("ExtensionsByType(%q) = %v, want %v", test.typ, ext, test.ext)
 		}
 	}
 }
